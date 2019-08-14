b41cb0ac61ee278294edb5537a92f5ce89817e93
Wersjon <Wersjon@users.noreply.github.com>
2019-08-14 18:21:45 +0200

    Mouser update 0.0.4alpha (from SAM)
    Updated Mouser functions from SAM

--- mouser.cpp
+++ mouser.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <cstdio>
 #include "mouser.hpp"
 
 using namespace std;
 
-void mouse()
+int mx, my; //Global variables for mouse X and mouse Y
+HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void mouse(void exist(), void click(), void doubleclick(),  void moving())
 {
-    int my, mx; //mouse position;
     INPUT_RECORD InRec; //Short of In Record, Describes an input event.
     DWORD EventCount; //Creates 32-bit unsigned integer.
     DWORD NumRead; //Creates 32-bit unsigned integer.
-    HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE); //I don't want write that monstrocity.
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE); //I don't want write that monstrocity.
+
+    bool doClick;
 
     while(true)
     {
         Sleep(10);
+        exist();
         GetNumberOfConsoleInputEvents(hIn, &EventCount); //Sets EventCount
         while(EventCount > 0)
         {
@@ -32,23 +37,54 @@ void mouse()
             }
             else if(InRec.EventType == MOUSE_EVENT) //If mouse caused an effect:
             {
-                if(InRec.Event.MouseEvent.dwEventFlags == 1 ) //if user DClicks:
+                if(InRec.Event.MouseEvent.dwEventFlags == 0) //If user clicks:
+                {
+                    doClick = (doClick)?0:1;
+                    my = InRec.Event.MouseEvent.dwMousePosition.Y; //Saves cursor position (Y)
+                    mx = InRec.Event.MouseEvent.dwMousePosition.X; //Saves cursor position (X)
+                    if(doClick == true)click();
+                }
+                else if(InRec.Event.MouseEvent.dwEventFlags == 2) //if user DClicks:
                 {
                     my = InRec.Event.MouseEvent.dwMousePosition.Y; //Saves cursor position (Y)
                     mx = InRec.Event.MouseEvent.dwMousePosition.X; //Saves cursor position (X)
-                    COORD c;
-                    c.X = 0; //Sets X coordinate.
-                    c.Y = 0; //Sets Y coordinate.
-                    SetConsoleCursorPosition(hOut, c); //Sets Cursor Position to c.X, c.Y.
-                    cout << "X:" << mx << "; Y:" << my << "; ";
+                    doubleclick();
                 }
-                else if (InRec.Event.MouseEvent.dwEventFlags == 0) //Gets where mouse is located:
+                else if(InRec.Event.MouseEvent.dwEventFlags == 1) //Gets where mouse is located, it needs to be in bottom.
                 {
                     my = InRec.Event.MouseEvent.dwMousePosition.Y; //Saves cursor position (Y)
-                    mx = InRec.Event.MouseEvent.dwMousePosition.X; //Saves cursor position (X)                    }
+                    mx = InRec.Event.MouseEvent.dwMousePosition.X; //Saves cursor position (X)
+                    moving();
                 }
             GetNumberOfConsoleInputEvents(hIn, &EventCount); //Gets Number of Console Input Events.
             }
         }
     }
+}
+
+void hidecursor() //Hides Cursor from user: (doesn't work if user resized window[?])
+{
+    CONSOLE_CURSOR_INFO CURSOR;
+    CURSOR.dwSize = 1;
+    CURSOR.bVisible = FALSE;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CURSOR);
+}
+
+void tp(int x, int y) //Teleports coordinates:
+{
+    COORD c; //Creates Coordinates variable named c.
+    c.X = x; //Sets X coordinate.
+    c.Y = y; //Sets Y coordinate.
+    SetConsoleCursorPosition(hOut, c); //Sets Cursor Position to c.X, c.Y.
+}
+
+void setColor(int bg, int c) //sets Console colors:
+{
+    SetConsoleTextAttribute(hOut, bg*16 + c);
+}
+
+void clear() //Clears 80x25 Screen:
+{
+    tp(0, 0);
+    printf("%2000d \n"); //Prints 2000 chars.
 }
